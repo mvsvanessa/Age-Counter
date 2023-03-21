ad4e9b791720db6891a495553ca2291fcfe654f7
Silva Vanessa (SO/OPM43-BR) <isa7ca@bosch.com>
2023-03-21 19:47:57 -0300

    maior idade

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MaiorIDade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(83, 182);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Insira a Idade";
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(83, 136);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.ReadOnly = true;
+            this.txtIdade.Size = new System.Drawing.Size(241, 20);
+            this.txtIdade.TabIndex = 2;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(168, 221);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Location = new System.Drawing.Point(249, 182);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "Próximo";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 311);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnIniciar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaiorIDade
+{
+    public partial class Form1 : Form
+    {
+        int maiorIdade = 0;  // Variável global
+        int pos = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            DialogResult dl = MessageBox.Show("Deseja Realmente Sair", "SAIR",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question); 
+            if (dl == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            txtIdade.ReadOnly = false;
+            btnNext.Enabled = true;
+            btnIniciar.Enabled = false;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (int.Parse(txtIdade.Text) > maiorIdade)
+                {
+                    maiorIdade = int.Parse(txtIdade.Text);
+                }
+                pos++;
+                txtIdade.Clear();
+                //txtIdade.Focus();
+               
+                if (pos <= 5)
+                {
+                    label1.Text = "Insira a Idade " + pos +":";
+                }
+               else
+                {
+                    txtIdade.ReadOnly = true;
+                    btnNext.Enabled = false;
+                    btnIniciar.Enabled = true;
+                    label1.Text = "Insira a Idade";
+                    MessageBox.Show("A Maior Idade é: " + maiorIdade, "MAIOR IDADE",
+                  MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                    
+                
+                                 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Insira sua Idade na caixa", "ERRO",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtIdade.Clear();
+               // txtIdade.Focus();
+            }
+            finally
+            {
+                txtIdade.Focus();
+            }
+        }
+    }
+}
